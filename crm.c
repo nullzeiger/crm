3b83e21ede496fd53f7c0236c9ff9dacfd4cdf4e
nullzeiger <ivan.guerreschi.dev@gmail.com>
2024-09-25 15:41:39 +0200

    Add source code

---
+++ crm.c
@@ -0,0 +1,64 @@
+/*
+  Copyright (C) 2024 Ivan Guerreschi <ivan.guerreschi.dev@gmail.com>.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <error.h>
+
+int
+main (int argc, char *argv[])
+{
+  if (argc != 2)
+    {
+      error (EXIT_FAILURE, 0, "Usage: %s <filename>", "crm");
+    }
+
+  const char *filename = argv[1];
+
+  if (access (filename, F_OK) == -1)
+    {
+      error (EXIT_FAILURE, errno, "File %s does not exist", filename);
+    }
+
+  char *command;
+  FILE *fp=NULL;
+  const char *mv = "cp %s %s";
+  const char *tmp = "/tmp";
+
+  command = malloc (sizeof (strlen (mv) + strlen (filename) + strlen (tmp)) + 1);
+  
+  sprintf (command, mv, filename, tmp);
+  
+  if ((fp = popen (command, "r")) == NULL)
+    {
+      error (EXIT_FAILURE, errno, "Error move %s in /tmp", filename);
+      pclose (fp);
+    }    
+
+  pclose (fp);
+  
+  if (unlink (filename) == -1)
+    {
+      error (EXIT_FAILURE, errno, "Error deleting file %s", filename);
+    }
+
+  printf ("File %s has been successfully deleted.\n", filename);
+  return EXIT_SUCCESS;
+}
