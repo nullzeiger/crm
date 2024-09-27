bc18432bb6b2b6848472fa6cac27ebc8cf49ee2f
nullzeiger <ivan.guerreschi.dev@gmail.com>
2024-09-27 09:28:08 +0200

    Add test

--- test_crm.pl
+++ test_crm.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 # Copyright (C) 2024 Ivan Guerreschi <ivan.guerreschi.dev@gmail.com>.
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by	
@@ -12,17 +14,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-crm: crm.c
-	gcc -Wall -Werror -ggdb -std=gnu11 -o crm crm.c
+use Test::More;
 
-clean: crm
-	rm crm
+my @args = qw(./crm test.txt);
 
-test:
-	@perl test_crm.pl
+# Create file test.txt.
+open my $fh, '>', $args[1];
+close $fh;
 
-# for emacs flymake
-.PHONY:
-	check-syntax
-check-syntax:
-	gcc -Wall -Werror -std=gnu11 -o /dev/null -S $(CHK_SOURCES) || true
+# Call ./crm test.txt.
+system(@args) == 0 or die "system @args failed: $?";
+
+my $file_exists = 0;
+
+# Check if file test.txt exixts in /tmp directory.
+if (-e "/tmp/$args[1]") {
+  $file_exists = 1;
+}
+
+# Check if file test.txt exixts in root directory.
+if (-e "$args[1]") {
+  $file_exists = 0;
+}
+
+# Exec test.
+ok($file_exists, "pass");
+
+# test done.
+done_testing();
